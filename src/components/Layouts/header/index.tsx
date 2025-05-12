f684e704148765507430e6b79e520d7823af4ded
Mrudul Ravi <115977212+mrudul-honorfx@users.noreply.github.com>
2025-05-12 20:53:58 +0400

    Update index.tsx

--- src/components/Layouts/header/index.tsx
+++ src/components/Layouts/header/index.tsx
@@ -35,14 +35,14 @@ export function Header() {
       )}
 
       <div className="max-xl:hidden">
-        <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
+        {/* <h1 className="mb-0.5 text-heading-5 font-bold text-dark dark:text-white">
           Dashboard
-        </h1>
+        </h1> */}
         
       </div>
 
       <div className="flex flex-1 items-center justify-end gap-2 min-[375px]:gap-4">
-        <div className="relative w-full max-w-[300px]">
+        {/* <div className="relative w-full max-w-[300px]">
           <input
             type="search"
             placeholder="Search"
@@ -52,7 +52,7 @@ export function Header() {
           <SearchIcon className="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2 max-[1015px]:size-5" />
         </div>
 
-        <ThemeToggleSwitch />
+        <ThemeToggleSwitch /> */}
 
         <Notification />
 
